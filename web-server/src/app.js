29ea68b70ac4df530ef39b771c21b90aa9d4fcd2
Filip <filipstevanovic176@gmail.com>
2020-11-10 21:50:34 +0100

    heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -14,63 +14,63 @@ const port = process.env.PORT || 8080
 // app.set('views', viewsPath)
 // hbs.registerPartials(partialsPath)
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '../../build')))
-  // Handle React routing, return all requests to React app
+// Handle React routing, return all requests to React app
 
-  // Setup static directory to serve
-  // app.use(express.static(publicDirectoryPath))
-  // app.use(express.static('public'))
-  // app.use(function(req, res, next) {
-  //     res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-  //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //     next();
-  //   });
+// Setup static directory to serve
+// app.use(express.static(publicDirectoryPath))
+// app.use(express.static('public'))
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
-  // app.get('/', (req, res) => {
-  //   // res.sendFile(path.join(__dirname, '../../frontend/weather-app/public' , 'build', 'index.html'))
-  //   res.send('lol')
-  // })
+// app.get('/', (req, res) => {
+//   // res.sendFile(path.join(__dirname, '../../frontend/weather-app/public' , 'build', 'index.html'))
+//   res.send('lol')
+// })
 
-  // app.get('/about', (req,res) => {
-  //     res.send('hey')
-  // })
+// app.get('/about', (req,res) => {
+//     res.send('hey')
+// })
 
-  // app.get('/help', (req,res) => {
-  //     res.render('help', {
-  //         title:'Help',
-  //         helpText:'This is some helpful text',
-  //         name:'Filip'
-  //     })
-  // })
+// app.get('/help', (req,res) => {
+//     res.render('help', {
+//         title:'Help',
+//         helpText:'This is some helpful text',
+//         name:'Filip'
+//     })
+// })
 
-  app.get('/weather/:address', (req, res) => {
-    const address = req.params.address
-    if (!address) {
-      return res.send({
-        error: 'You must provide an address!',
-      })
+app.get('/weather/:address', (req, res) => {
+  const address = req.params.address
+  if (!address) {
+    return res.send({
+      error: 'You must provide an address!',
+    })
+  }
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error: 'EH VRE' })
     }
 
-    geocode(address, (error, { latitude, longitude, location } = {}) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-        return res.send({ error: 'EH VRE' })
+        return res.send({ error: 'EEEEEEEEEJ' })
       }
-
-      forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-          return res.send({ error: 'EEEEEEEEEJ' })
-        }
-        res.send({
-          forecast: forecastData,
-          location,
-          address: req.query.address,
-        })
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
       })
     })
   })
+})
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../build')))
   app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname + '../../build/index.html'))
   })
