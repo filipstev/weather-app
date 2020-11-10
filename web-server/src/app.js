da770695632e95b3f1105db3aef050be2656431a
Filip <filipstevanovic176@gmail.com>
2020-11-10 21:16:00 +0100

    heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -14,6 +14,15 @@ const publicDirectoryPath = path.join(__dirname, '..', 'build')
 // app.set('views', viewsPath)
 // hbs.registerPartials(partialsPath)
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../build')))
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  })
+}
+
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 app.use(express.static('public'))
