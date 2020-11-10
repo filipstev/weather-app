74f5906ed81f9e14b510939dd748e7b2f8438706
Filip <filipstevanovic176@gmail.com>
2020-11-10 21:39:50 +0100

    heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -72,7 +72,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '../../build/index.html'))
+    res.sendFile(path.resolve(__dirname + '../../build/index.html'))
   })
 }
 // app.get('/products',(req, res) => {
